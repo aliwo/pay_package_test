f8d3fafd5f9675081805fcc7e0fe529264e0f8a5
SW <aliwo@naver.com>
2024-10-11 13:02:34 +0900

    add simple package

---
+++ pay.py
@@ -0,0 +1,16 @@
+
+
+class Pay:
+
+    def __init__(self, database_reader_url: str, database_writer_url: str) -> None:
+        self.database_writer_url = database_writer_url
+        self.database_reader_url = database_reader_url
+
+    def reserve(self, payment_no: str) -> None:
+        print(f"[reserve]{payment_no}")
+
+    def checkout(self, payment_no: str) -> None:
+        print(f"[checkout]{payment_no}")
+
+    def approve(self, payment_no: str) -> None:
+        print(f"[approve]{payment_no}")
